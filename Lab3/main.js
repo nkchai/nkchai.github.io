4e5668f4f2a2c5eafc741e6b4787b8505e6c034e
nkchai <156144386+nkchai@users.noreply.github.com>
2024-11-03 12:42:32 -0600

    added currency conversion

--- Lab3/main.js
+++ Lab3/main.js
@@ -3,12 +3,17 @@ const errmessagebt = document.getElementById("errormessagebt");
 const tip_slider = document.getElementById('tipslider');
 const tip_per_input = document.getElementById('tippercentinput');
 const errmessageslider = document.getElementById('errormessageslider');
-const tipamount = document.getElementById("converted tip amount")
-const total_final = document.getElementById("Converted Total Bill with Tip")
+const tipamount = document.getElementById("converted tip amount");
+const total_final = document.getElementById("Converted Total Bill with Tip");
+const currency = document.getElementById("Currency");
+const symbol = document.getElementById("symbol");
+const symbol1 = document.getElementById("symbol1");
 errmessageslider.style.display = 'none';
 tip_slider.disabled = true;
 errmessagebt.style.display = 'none'
-
+let camount = 1;
+let bill1 = 0;
+currency.disabled = true;
 
 bill_total.addEventListener('input', function () {
 
@@ -17,6 +22,7 @@ bill_total.addEventListener('input', function () {
     if (!isNaN(bill) && bill > 0) {
         errmessagebt.style.display = 'none'
         tip_slider.disabled = false;
+        currency.disabled = false;
         // tip_per_input.disabled = false;
         convertbill(bill);
         tip_slider.addEventListener('input', function () {
@@ -25,25 +31,47 @@ bill_total.addEventListener('input', function () {
             convertbill(bill);
         })
 
-        tip_per_input.addEventListener('input', function () {
-            errmessageslider.style.display = 'none';
-            let value = parseInt(tip_per_input.value, 10);
+        // tip_per_input.addEventListener('input', function () {
+        //     errmessageslider.style.display = 'none';
+        //     let value = parseInt(tip_per_input.value, 10);
 
-            if (value < 0) {
-                value = 0;
-                errmessageslider.textContent = "Number must be positive";
-                errmessageslider.style.display = 'inline'
-            }
-            if (value > 100) {
-                value = 100;
-                errmessageslider.textContent = "Number must be less than 100";
-                errmessageslider.style.display = 'inline'
-            }
-            tip_slider.value = value;
-            // tipamount.innerHTML = value
-            // convertbill(bill);
+        //     if (value < 0) {
+        //         value = 0;
+        //         errmessageslider.textContent = "Number must be positive";
+        //         errmessageslider.style.display = 'inline'
+        //     }
+        //     if (value > 100) {
+        //         value = 100;
+        //         errmessageslider.textContent = "Number must be less than 100";
+        //         errmessageslider.style.display = 'inline'
+        //     }
+        //     tip_slider.value = value;
+        //     // tipamount.innerHTML = value
+        //     // convertbill(bill);
+       
            
-        });
+        // });
+        currency.addEventListener('input',function(){
+            if (currency.value === "USD"){ 
+                camount = 1;
+                symbol.textContent='$';
+                symbol1.textContent='$';
+                convertbill(bill);
+            }
+            else if (currency.value === "INR"){
+                camount = 84.07;
+                symbol.textContent='₹';
+                symbol1.textContent='₹';
+                convertbill(bill);
+            }
+            else if (currency.value === "YEN") {
+                camount = 149.34;
+                symbol.textContent='¥';
+                symbol1.textContent='¥';
+                convertbill(bill);
+            }
+            
+        })
 
     }
     else {
@@ -55,14 +83,17 @@ bill_total.addEventListener('input', function () {
         tipamount.value = "";
         tip_slider.value = 0;
         tip_per_input.value = 0;
+        currency.disabled = true;
         // convertbill(bill);
     }
 
     function convertbill(bill) {
+        
+        bill1 = bill * camount;
         const tip_percent = parseFloat(tip_per_input.value, 10);
-        const tipAmount = (bill * (tip_percent / 100)).toFixed(2); // Calculate tip amount
+        const tipAmount = (bill1 * (tip_percent / 100)).toFixed(2); // Calculate tip amount
         tipamount.value = tipAmount; // Update the displayed converted tip amount
-        total_final.value = (parseFloat(bill) + parseFloat(tipAmount)).toFixed(2);
+        total_final.value = (parseFloat(bill1) + parseFloat(tipAmount)).toFixed(2);
 
     }
 });
